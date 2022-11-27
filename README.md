268e6c1279221a09817657cde051173d6853b49b
RyougikiMikiya <2467281535@qq.com>
2022-11-27 16:12:28 +0800

    hello world

--- README.md
+++ README.md
@@ -1,1 +1,104 @@
-# uring_learn
+# io_uring初探
+
+## 前言
+
+和网卡收包一样，硬件性能越来越强的今天，操作系统的传统API已经成了非常大的瓶颈。比如write虽然不是阻塞操作，但是依然会陷入内核然后巴拉巴拉搞一堆，这个就看看文档就知道了。
+同样的现在流行的SPDK也是把dpdk搬到了存储上，SPDK我还没用过不做介绍。而异步IO一直是Linux的痛点，AIO风评非常的差，随便一搜都是吐槽（没在项目中用过，不做评论。。）。所以io_uring横空出世就是为了解决目前这个问题。他需要高内核版本支持，并且每个发行版本可能还不一样。但是全是5.x+，否则`io_uring_queue_init`会返回-ENOSYS，也就是没有这个系统调用。网上的中文文章有，但是看着很头痛。。。还是看看原生的英文教学吧家人们。<https://unixism.net/loti/tutorial/index.html> loti的缩写是lord of the io_uring。非常的牛逼~~
+
+## io_uring和liburing
+
+简单的说，io_uring的原生api非常的复杂，虽然他只有三个系统调用，但是用好是非常困难的。所以我们的大神亲切的为我们封装了一个对调用者友好的库，那就是liburing。在实际中我们只需要调用liburing的接口就可以相对轻松的实现异步io操作了。liburing几乎封装了所有的io操作，不仅限于磁盘还有网络。官方教程中还有一个echo server后面有兴趣可以继续研究。
+
+## 环境搭建
+
+WSL2：翻微软的Github release可知，WSL2直到5.10.60.1-microsoft-standard-WSL2才支持io_uring功能。其他的平台暂时没测
+WSL2可以去 <https://www.catalog.update.microsoft.com/Search.aspx?q=wsl>下载update然后一键升级，很方便
+而liburing的地址是<https://github.com/axboe/liburing> 克隆后make && make install即可
+
+## 性能表现
+
+测试文件：39M test.pcap
+
+测试代码: uring_test.cpp
+
+```plain
+iovecs size 113411
+total pkts: 56705
+writev Elapsed: 19414 clock
+write Elapsed: 128066 clock
+fstream Elapsed: 43908 clock
+uring1 Elapsed: 827 clock
+uring2 Elapsed: 1894 clock
+```
+
+可以看到顺序写的性能非常强劲
+
+## 基本原理
+
+岂敢岂敢，只能写一下使用心得。可能还有误
+
+在liburing中，我们要使用到两个队列来完成我们的异步操作。一个叫Submit Queue，一个叫Completion Queue。从字面意思可以很简单的看出，一个是提交队列，一个是完成队列。这两个队列是来自于我们初始化的一个uring。当用户要sq提交异步操作并确认后便把工作交给了内核来处理，只需要在“合适”的时机来检查一下cq里面是有完成事件即可。你可能会问检测（wait）操作同样要阻塞那异步的价值在哪里呢？但是参考测试代码和结果你就能发现原来传统io耗费了太多的时间导致异步操作的性能提升了十倍不止（仅本用例）。
+
+## SQE与Submit
+
+```c
+/* Init a uring */
+...
+/* Get an SQE */
+struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
+/* Setup a readv operation */
+io_uring_prep_readv(sqe, file_fd, fi->iovecs, blocks, 0);
+/* Set user data */
+io_uring_sqe_set_data(sqe, fi);
+/* Finally, submit the request */
+io_uring_submit(ring);
+```
+
+必须值得注意的是，每一次io操作必须绑定一个sqe，一次submit可以同时submit多次io操作。同时最大支持提交的io操作取决于初始化时指定的队列大小`io_uring_queue_init`的第一次参数。笔者最开始学习时在一个sqe上反复提交了多个writev操作最后发现只执行了一次在这里踩了大坑，所以记得多看文档比如
+
+`io_uring_submit`：
+
+```
+Submits the SQEs acquired via io_uring_get_sqe() to the kernel. You can call this once after you have called io_uring_get_sqe() multiple times to set up multiple I/O requests.
+```
+
+`io_uring_get_sqe`:
+
+```
+This function returns a submission queue entry that can be used to submit an I/O operation. You can call this function multiple times to queue up I/O requests before calling io_uring_submit() to tell the kernel to process your queued requests.
+```
+
+## CQE与Completion
+
+```c
+int get_completion_and_print(struct io_uring *ring) {
+    struct io_uring_cqe *cqe;
+    int ret = io_uring_wait_cqe(ring, &cqe);
+
+    if (ret < 0) {
+        perror("io_uring_wait_cqe");
+        return 1;
+    }
+
+    if (cqe->res < 0) { // io操作的返回值
+        /* The system call invoked asynchonously failed */
+        return 1;
+    }
+
+    /* Retrieve user data from CQE */
+    struct file_info *fi = io_uring_cqe_get_data(cqe);
+    /* process this request here */
+
+    /* Mark this completion as seen */
+    io_uring_cqe_seen(ring, cqe);
+    return 0;
+}
+```
+
+对于每一个wait的操作，在处理完毕后，必须调用seen或者类似的函数来显示的清空他。不然下次检测还会检测到他。我在程序中还调用了诸如`io_uring_submit_and_wait`这样一气呵成的代码，有点类似rust中`async+await?`的感觉吧。
+
+## 总结
+
+个人粗略的学习之后认为，异步开发比同步在代码设计上难度确实会大很多，主要就是sqe和cqe的管理，sqe满和不满时不同的操作，cqe返回时的状态检测等等。但是性能提升应该是肉眼可见的。
+
+一个线程在sqe可以获取时，submit一个已经组装好的iovecs。而sqe不能获取时或者没有数据时，则组装数据。另一个线程则纯粹的等待cqe，做返回与善后处理。
---
+++ uring_pcap_copy.cpp
@@ -0,0 +1,386 @@
+#include <array>
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <sys/uio.h>
+#include <vector>
+
+#include <cassert>
+#include <limits.h>
+#include <pcap.h>
+#include <cstring>
+#include <cstdlib>
+#include <memory.h>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <liburing.h>
+
+const static pcap_file_header std_pcap_hdr = {.magic = 0xa1b2c3d4,
+                                              .version_major = 2,
+                                              .version_minor = 4,
+                                              .thiszone = 0,
+                                              .sigfigs = 0,
+                                              .snaplen = 0x00040000,
+                                              .linktype = 1};
+
+using namespace std;
+
+void test01(int argc, char **argv)
+{
+    string errBuf;
+    errBuf.reserve(PCAP_ERRBUF_SIZE);
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_dumper_t *dumper = nullptr;
+    pcap_t *pcap = pcap_open_offline(argv[1], errbuf);
+    if (!pcap)
+    {
+        cout << errBuf << endl;
+        goto exit;
+    }
+
+    dumper = pcap_dump_open(pcap, "test.pcap");
+    if (!dumper)
+    {
+        perror("pcap_dump_open");
+        goto exit;
+    }
+
+    pcap_loop(pcap, 0, pcap_dump, (u_char *)dumper);
+
+    pcap_dump_close(dumper);
+
+exit:
+    return;
+}
+
+struct my_pcap_sf_pkthdr
+{
+    bpf_int32 tv_sec;   /* seconds */
+    bpf_int32 tv_usec;  /* microseconds */
+    bpf_u_int32 caplen; /* length of portion present */
+    bpf_u_int32 len;    /* length of this packet (off wire) */
+    my_pcap_sf_pkthdr(const struct pcap_pkthdr *hdr)
+        : len(hdr->len), caplen(hdr->caplen), tv_sec((bpf_int32)hdr->ts.tv_sec),
+          tv_usec(bpf_int32(hdr->ts.tv_usec)) {}
+};
+
+using pkthdr_vec = std::vector<my_pcap_sf_pkthdr>;
+using pktdata = std::vector<u_char>;
+using pktdata_vec = std::vector<pktdata>;
+
+pkthdr_vec pkthdrs;
+pktdata_vec pktdatas;
+
+void my_handler(u_char *user, const struct pcap_pkthdr *hdr,
+                const u_char *data)
+{
+    pcap_dump(user, hdr, data);
+    pkthdrs.emplace_back(hdr);
+    pktdatas.emplace_back(pktdata(data, data + hdr->caplen));
+}
+
+std::vector<iovec> iovecs;
+
+/*
+      i = 0, j = 1,2
+      i = 1, j = 3,4
+      i = 2, j = 5,6
+ */
+
+void prepare_iovec()
+{
+    iovecs.clear();
+    assert(pkthdrs.size() == pktdatas.size());
+    // int cnt = min(pkthdrs.size(), pktdatas.size());
+    int cnt = pkthdrs.size();
+    iovecs.reserve(cnt * 2 + 1);
+
+    iovecs.push_back(
+        {.iov_base = (void *)&std_pcap_hdr,
+         .iov_len = sizeof std_pcap_hdr});
+    for (int i = 0; i != cnt; i++)
+    {
+        iovecs.push_back({.iov_base = (void *)&pkthdrs[i],
+                          .iov_len = sizeof(my_pcap_sf_pkthdr)});
+
+        iovecs.push_back({.iov_base = (void *)pktdatas[i].data(),
+                          .iov_len = pktdatas[i].size()});
+    }
+    cout << "iovecs size " << iovecs.size() << endl;
+}
+
+void write_pcap_by_writev(int fd)
+{
+    assert(fd >= 0);
+
+    int writed = 0, rest = iovecs.size();
+    do
+    {
+        int per = rest > IOV_MAX ? IOV_MAX : rest;
+        int ret = writev(fd, iovecs.data() + writed, per);
+        if (ret < 0)
+        {
+            perror("writev\n");
+            break;
+        }
+        writed += per;
+        rest -= IOV_MAX;
+    } while (rest > 0);
+}
+
+void write_pcap_by_write(int fd2)
+{
+    assert(fd2 >= 0);
+
+    int ret = write(fd2, &std_pcap_hdr, sizeof std_pcap_hdr);
+    assert(ret > 0);
+
+    for (int i = 0; i < pkthdrs.size(); i++)
+    {
+        ret = write(fd2, &pkthdrs[i], sizeof(my_pcap_sf_pkthdr));
+        assert(ret > 0);
+        int len1 = pktdatas[i].size();
+        ret = write(fd2, pktdatas[i].data(), len1);
+        assert(ret > 0);
+    }
+}
+
+void write_pcap_by_fstream(ofstream &f1)
+{
+    assert(f1.is_open());
+
+    f1.write((const char *)&std_pcap_hdr, sizeof std_pcap_hdr);
+
+    for (int i = 0; i < pkthdrs.size(); i++)
+    {
+        f1.write((const char *)&pkthdrs[i], sizeof(my_pcap_sf_pkthdr)).write((const char *)pktdatas[i].data(), pktdatas[i].size());
+        if (f1.bad())
+        {
+            assert(false);
+        }
+    }
+}
+
+void write_pcap_by_uring_1(int fd)
+{
+    assert(fd >= 0);
+    int ret;
+    struct io_uring ring;
+    int cur_offset = 0;
+
+    uint entries = 16;
+
+    ret = io_uring_queue_init(entries, &ring, 0);
+    if (ret < 0)
+    {
+        perror("io_uring_queue_init !!");
+        return;
+    }
+
+    iovec *tmp = iovecs.data();
+    int size = iovecs.size();
+
+    struct io_uring_cqe *cqe = nullptr;
+    int writed = 0, rest = iovecs.size();
+    int per = rest > IOV_MAX ? IOV_MAX : rest;
+    int submited = 0;
+
+    do
+    {
+        auto sqe = io_uring_get_sqe(&ring);
+        if (!sqe)
+        {
+#if 0
+            ret = io_uring_submit(&ring);
+            printf("io_uring_submit ret %d\n", ret);
+            int pending = ret;
+            for (int i = pending; i != 0; i--)
+            {
+                ret = io_uring_wait_cqe(&ring, &cqe);
+                if (ret < 0)
+                {
+                    fprintf(stderr, "io_uring_wait_cqe: %s\n",
+                            strerror(-ret));
+                    return;
+                }
+                io_uring_cqe_seen(&ring, cqe);
+            }
+#else
+            ret = io_uring_submit_and_wait(&ring, entries);
+            /*
+                如果ret 不等于 entries...要怎么办？应该是again
+             */
+            assert(ret == entries);
+            io_uring_cq_advance(&ring, ret);
+#endif
+            submited = 0;
+            continue;
+        }
+        int per = rest > IOV_MAX ? IOV_MAX : rest;
+        /* 
+            这里必须要自己去算offset，不能用-1。更不能用0！！-1代表append。0就永远在开头写
+         */
+        io_uring_prep_writev(sqe, fd, iovecs.data() + writed, per, cur_offset);
+        for (int i = writed; i < writed + per; i++)
+        {
+            cur_offset += iovecs[i].iov_len;
+        }
+        writed += per;
+        rest -= per;
+        submited++;
+    } while (rest > 0);
+
+    ret = io_uring_submit_and_wait(&ring, submited);
+    assert(ret == submited);
+    io_uring_cq_advance(&ring, ret);
+
+    io_uring_queue_exit(&ring);
+
+    return;
+}
+
+void write_pcap_by_uring_2(int fd)
+{
+    int ret;
+    struct io_uring ring;
+    int cur_offset = 0;
+
+    uint entries = 8;
+
+    ret = io_uring_queue_init(entries, &ring, 0);
+    if (ret < 0)
+    {
+        cout << ret << endl;
+        perror("io_uring_queue_init !!");
+        return;
+    }
+
+    iovec *tmp = iovecs.data();
+    int size = iovecs.size();
+
+    struct io_uring_cqe *cqe = nullptr;
+    int writed = 0, rest = iovecs.size();
+    int per = rest > IOV_MAX ? IOV_MAX : rest;
+    int submited = 0;
+
+    do
+    {
+        auto sqe = io_uring_get_sqe(&ring);
+        if (!sqe)
+        {
+            /* 
+            大概不可能
+             */
+            assert(false);
+        }
+        int per = rest > IOV_MAX ? IOV_MAX : rest;
+        io_uring_prep_writev(sqe, fd, iovecs.data() + writed, per, cur_offset);
+        int ret = io_uring_submit(&ring);
+        if (ret < 0) {
+            fprintf(stderr, "io_uring_submit: %s\n",
+                    strerror(-ret));
+            return; 
+        }
+        for (int i = writed; i < writed + per; i++)
+        {
+            cur_offset += iovecs[i].iov_len;
+        }
+        writed += per;
+        rest -= per;
+        submited++;
+    } while (rest > 0);
+
+    for (int pending = submited; pending > 0; pending--) {
+        /* 
+            非阻塞用 io_uring_peek_cqe
+         */
+        ret = io_uring_wait_cqe(&ring, &cqe);
+        if (ret < 0) {
+            fprintf(stderr, "io_uring_wait_cqe: %s\n",
+                    strerror(-ret));
+            return;             
+        }
+        io_uring_cqe_seen(&ring, cqe);
+    }
+
+    io_uring_queue_exit(&ring);
+    return;
+}
+
+int main(int argc, char **argv)
+{
+    string errBuf;
+    errBuf.reserve(PCAP_ERRBUF_SIZE);
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_dumper_t *dumper = nullptr;
+
+    pcap_t *pcap = pcap_open_offline(argv[1], errbuf);
+    if (!pcap)
+    {
+        cout << "需要传一个pcap文件~" << errBuf << endl;
+        return -1;
+    }
+
+    auto file = pcap_dump_open(pcap, "test_pcap_dump.pcap");
+    pcap_loop(pcap, 0, my_handler, (u_char*)file);
+
+    pcap_close(pcap);
+
+    /*
+        准备iovec数据
+      */
+
+    prepare_iovec();
+
+    cout << "total pkts: " << pkthdrs.size() << endl;
+
+    int fd = open("test_writev.pcap", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    clock_t tic1 = clock();
+    write_pcap_by_writev(fd);
+    clock_t tic2 = clock();
+    close(fd);
+
+    printf("writev Elapsed: %lu clock\n", tic2 - tic1);
+
+    int fd2 = open("test_writev.pcap", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    clock_t tic3 = clock();
+    write_pcap_by_write(fd2);
+    clock_t tic4 = clock();
+    close(fd2);
+
+    printf("write Elapsed: %lu clock\n", tic4 - tic3);
+
+    ofstream f1("test_fstream.pcap", ios::out | ios::binary | ios::trunc);
+    clock_t tic5 = clock();
+    write_pcap_by_fstream(f1);
+    clock_t tic6 = clock();
+    f1.close();
+
+    printf("fstream Elapsed: %lu clock\n", tic6 - tic5);
+
+
+    fd = open("test_uring_1.pcap", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    clock_t tic7 = clock();
+    write_pcap_by_uring_1(fd);
+    clock_t tic8 = clock();
+    close(fd);
+
+    printf("uring1 Elapsed: %lu clock\n", tic8 - tic7);
+
+    fd = open("test_uring_2.pcap", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    clock_t tic9 = clock();
+    write_pcap_by_uring_2(fd);
+    clock_t tic10 = clock();
+    close(fd);
+
+    printf("uring2 Elapsed: %lu clock\n", tic10 - tic9);
+
+exit:
+    return 0;
+}
